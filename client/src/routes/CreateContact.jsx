3bd1b969da451d12fca9f4c1da7cc7515e607578
igachev <ivophoenix1135@gmail.com>
2024-04-04 17:20:56 +0300

    added form for creating a contact

--- client/src/routes/CreateContact.jsx
+++ client/src/routes/CreateContact.jsx
@@ -1,10 +1,38 @@
+import { Form } from "react-router-dom";
 
 export default function CreateContact() {
 
     return (
         <div>
             <h2>Create Contact</h2>
+            <Form method="post">
+
+            <div>
+                <label htmlFor="firstName">First Name:</label>
+                <input type="text" name="firstName" />
+            </div>
+
+            <div>
+                <label htmlFor="lastName">Last Name:</label>
+                <input type="text" name="lastName" />
+            </div>
+
+            <div>
+                <label htmlFor="imageUrl">Image URL:</label>
+                <input type="text" name="imageUrl" />
+            </div>
+
+            <div>
+                <label htmlFor="notes">Notes:</label>
+                <textarea name="notes" cols="30" rows="10"></textarea>
+            </div>
+
+            <div>
+                <input type="submit" value="Create" />
+            </div>
+
+            </Form>
         </div>
     )
-    
+
 }
